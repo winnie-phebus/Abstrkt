4eefaf605b76798503c4e16c954affbad85fc48f
Alix Heudebourg <alix.heudebourg@gmail.com>
2022-06-20 12:17:55 -0400

    Folder Update

--- app/src/main/java/com/example/abstrkt/FolderHolder.java
+++ app/src/main/java/com/example/abstrkt/FolderHolder.java
@@ -2,11 +2,14 @@ package com.example.abstrkt;
 
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,33 +18,18 @@ import android.view.ViewGroup;
  */
 public class FolderHolder extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ImageView folderIcon;
+    TextView folderName;
+    ConstraintLayout layout;
 
     public FolderHolder() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FolderHolder.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static FolderHolder newInstance(String param1, String param2) {
         FolderHolder fragment = new FolderHolder();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +37,31 @@ public class FolderHolder extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        if (getArguments() != null) {}
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.preview_folder, container, false);
+        View rootView =  inflater.inflate(R.layout.preview_folder, container, false);
+
+        folderIcon = rootView.findViewById(R.id.folder_prev_icon);
+        folderName = rootView.findViewById(R.id.folder_prev_name);
+
+        layout = rootView.findViewById(R.id.folder_outsideContainer);
+
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO on click open a recylcer view below with all notes in that folder
+                // change state of foler to open -> change icon to folder_open ressource file
+                // click to close
+            }
+        });
+
+
+
+        return rootView;
     }
 }
